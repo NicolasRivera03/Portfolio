56be3aaccd8408b9769878eb13137013221c8a07
Nicolas Rivera <riveramoranicolas@gmail.com>
2023-06-28 14:00:02 -0500

    Selected Work section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import HeroSection from "@/organisms/HeroSection/HeroSection";
+import SelectedWork from "@/organisms/SelectedWork/SelectedWork";
 
 export default function Home() {
   return (
     <main className="">
       <HeroSection />
+      <SelectedWork />
     </main>
   );
 }
---
+++ src/organisms/SelectedWork/SelectedWork.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SelectedWork = () => {
+  return (
+    <div className="flex items-center w-full h-screen">
+      <h2>Selected Work</h2>
+    </div>
+  );
+};
+
+export default SelectedWork;
