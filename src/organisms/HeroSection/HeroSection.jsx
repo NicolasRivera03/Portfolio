5cce1ea6ee9b0955028eacd783fed6d2821a59df
nico <nico@nicos-MacBook-Air.local>
2023-06-28 13:48:29 -0500

    Reviewed logo and fonts

--- src/organisms/HeroSection/HeroSection.jsx
+++ src/organisms/HeroSection/HeroSection.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const HeroSection = () => {
   return (
     <div className="sm:px-2 py-2 h-screen w-full md:h-screen w-full px-40 py-10 grid place-content-center gap-2 relative">
-      <div className="absolute xs:top-[14%] top-[20%]  md:top-48 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20">
+      <div className="grid place-content-center -mb-16">
         <Image
           src="/Memongi.png"
           width={230}
@@ -14,7 +14,8 @@ const HeroSection = () => {
         />
       </div>
       <h1 className=" text-center font-extrabold relative max-[700px]:text-7xl inline-block tracking-tighter text-transparent text-8xl bg-clip-text bg-gradient-to-b dark:from-[#7ef29d] dark:to-[#0f68a9] from-[#7ef29d] to-[#0f68a9]">
-        ¡Hola!, Soy Nicolás
+        <span>¡Hola!</span>
+        <span>, Soy Nicolás</span>
       </h1>
       <p className="text-center dark:text-[#cecece] text-neutral-700 tracking-tight font-semibold text-2xl">
         Frontend software engineer and interface designer
