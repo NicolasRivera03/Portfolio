af407febb2d13500f927095cebfb37b0881e7d3a
Nicolás Rivera Mora <93621580+nicolasriveranivelics@users.noreply.github.com>
2023-06-28 22:37:08 -0500

    h2

--- src/organisms/SelectedWork/SelectedWork.jsx
+++ src/organisms/SelectedWork/SelectedWork.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const SelectedWork = () => {
   return (
-    <div className="flex items-center w-full h-screen">
-      <h2>Selected Work</h2>
+    <div className="grid place-content-center w-full h-screen">
+      <h2 className="font-bold text-[#1f1c19] dark:text-[#cdbead] relative item-s text-transparent text-6xl bg-clip-text inline-block text-gradient tracking-tighter">
+        Selected Work
+      </h2>
+      <div className="grid grid-cols-2 bg-slate-300"></div>
     </div>
   );
 };
