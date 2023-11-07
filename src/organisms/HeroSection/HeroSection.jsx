9c02e48e5a3df7b899f83548338d64bf8332edc8
Nico <nico@Nicos-MacBook-Air.local>
2023-11-07 07:36:06 -0500

    Chaged hero section

--- src/organisms/HeroSection/HeroSection.jsx
+++ src/organisms/HeroSection/HeroSection.jsx
@@ -1,46 +1,78 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+import { useFollowPointer } from "./use-follow-pointer";
 
 const HeroSection = () => {
+  const ref = useRef(null);
+  const { x, y } = useFollowPointer(ref);
+
   return (
-    <div className="sm:px-2 py-2 h-screen w-full md:h-screen md:px-40 md:py-10 grid place-content-center gap-2 relative">
-      <div className="grid place-content-center -mb-16">
-        <Image
-          src="/Memongi.png"
-          width={230}
-          height={230}
-          alt="Memongi Nicolás Rivera Mora"
-        />
-      </div>
-      <h1 className=" text-center font-extrabold relative max-[700px]:text-7xl inline-block tracking-tighter text-transparent text-8xl bg-clip-text bg-gradient-to-b dark:from-[#7ef29d] dark:to-[#0f68a9] from-[#7ef29d] to-[#0f68a9]">
-        <span>Hi!</span>
-        <span>, Im Nico</span>
-      </h1>
-      <p className="text-center dark:text-[#cecece] text-neutral-700 tracking-tight font-semibold text-2xl">
-        Frontend software engineer and interface designer
-      </p>
-      <div className="flex gap-4 items-center justify-center pt-2">
-        <a
-          href="mailto:riveramoranicolas@gmail.com"
-          className="p-4 bg-emerald-50 rounded text-slate-950 font-black"
-        >
-          Contactame
-        </a>
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="box sm:px-2 py-2 h-screen w-full md:h-screen md:px-40 md:py-10 grid place-content-center gap-2 relative bg-slate-950"
+      >
+        <div className="grid place-content-center -mb-16">
+          <Image
+            src="/Memongi.png"
+            width={230}
+            height={230}
+            alt="Memongi Nicolás Rivera Mora"
+          />
+        </div>
+        <h1 className="text-center font-extrabold relative max-[700px]:text-7xl inline-block tracking-tighter text-transparent text-8xl bg-clip-text bg-gradient-to-b dark:from-[#7ef29d] dark:to-[#0f68a9] from-[#7ef29d] to-[#0f68a9]">
+          <span>Hola!</span>
+          <span>, soy Nico</span>
+        </h1>
+        <p className="text-center bg-gray-900s  text-slate-50 tracking-tight font-medium text-2xl">
+          Desarrollador Frontend y Diseñador Web
+        </p>
+        <div className="flex gap-4 items-center justify-center pt-2">
+          <motion.a
+            whileHover={{ scale: 1.2 }}
+            onHoverStart={(e) => {}}
+            onHoverEnd={(e) => {}}
+            href="mailto:riveramoranicolas@gmail.com"
+            className="p-4 z-10 bg-slate-900 text-white rounded-full  font-semibold"
+          >
+            Contactame
+          </motion.a>
 
-        <a
-          href="https://resume.io/r/0my4lDyLi"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="p-4 outline outline-2 rounded font-black">
-            Mi CV
-          </button>
-        </a>
-      </div>
+          <motion.a
+            whileHover={{ scale: 1.2 }}
+            onHoverStart={(e) => {}}
+            onHoverEnd={(e) => {}}
+            href="https://resume.io/r/0my4lDyLi"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="z-10 p-4 outline outline-white text-white outline-2 rounded-full font-semibold">
+              Mi CV
+            </button>
+          </motion.a>
+        </div>
 
-      <div className="text-2xl absolute bottom-10 md:bottom-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20"></div>
-    </div>
+        <div className="text-2xl absolute bottom-10 md:bottom-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20"></div>
+      </motion.div>
+      <motion.div
+        ref={ref}
+        className="fixed bg-transparent z-0 outline-dashed outline-white w-24 h-24 rounded-full "
+        animate={{ x, y }}
+        transition={{
+          type: "spring",
+          damping: 2,
+          stiffness: 30,
+          restDelta: 0.001,
+        }}
+      />
+    </>
   );
 };
 
--- src/organisms/SelectedWork/SelectedWork.jsx
+++ src/organisms/SelectedWork/SelectedWork.jsx
@@ -1,144 +1,155 @@
+"use client";
+
 import React from "react";
 import { BiLogoReact } from "react-icons/bi";
+import { motion } from "framer-motion";
 
 const SelectedWork = () => {
   return (
-    <div className="flex flex-col justify-center w-full h-auto px-4 md:px-10 max-w-screen-xl m-auto">
-      <p className="text-center">What i have done so far</p>
-      <h2 className="text-center font-bold text-[#1f1c19] dark:text-[#cdbead] relative item-s text-transparent text-6xl bg-clip-text inline-block text-gradient tracking-tighter">
-        Employment History
-      </h2>
-      <div className="grid  bg-slate-100 dark:bg-gray-900 mt-5 rounded p-4">
-        <div className="flex flex-col gap-4">
-          <div className="bg-white dark:bg-slate-800 rounded p-4">
-            <h3 className="dark:text-[#cdbead] text-[#1f1c19] text-2xl font-bold tracking-tighter">
-              Frontend Developer at Farmalógica, Bogotá
-            </h3>
-            <p>
-              Experience developing an application what integrates SAP BO, with
-              React API Fetch Tailwind and SQL Server.
-            </p>
-            <p className="dark:text-[#cdbead] text-[#1f1c19] text-1xl font-thin tracking-tighter">
-              March 2023 - Present
-              <span></span>
-            </p>
-            <ul className="pt-2 list-disc pl-4">
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Analyze and optimize sells and invoices processes
-              </li>
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Create APIs with Node JS
-              </li>
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Design Optimized process with Figma
-              </li>
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Design Low fidelity designs{" "}
-              </li>
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Design High fidelity designs
-              </li>
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Design SQL model from Database.
-              </li>
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Documentation code.
-              </li>
-            </ul>
+    <div className="w-full bg-slate-950">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        className="flex flex-col justify-center w-full h-auto px-4 md:px-10 max-w-screen-xl m-auto bg-slate-900"
+      >
+        <p className="text-center">What i have done so far</p>
+        <h2 className="text-center font-bold text-[#cdbead] relative item-s text-transparent text-6xl bg-clip-text inline-block text-gradient tracking-tighter">
+          Employment History
+        </h2>
+        <div className="grid  bg-slate-100 dark:bg-gray-900 mt-5 rounded p-4">
+          <div className="flex flex-col gap-4">
+            <div className="bg-white dark:bg-slate-800 rounded p-4">
+              <h3 className="text-[#cdbead] text-2xl font-bold tracking-tighter">
+                Frontend Developer at Farmalógica, Bogotá
+              </h3>
+              <p>
+                Experience developing an application what integrates SAP BO,
+                with React API Fetch Tailwind and SQL Server.
+              </p>
+              <p className="dark:text-[#cdbead] text-[#1f1c19] text-1xl font-thin tracking-tighter">
+                March 2023 - Present
+                <span></span>
+              </p>
+              <ul className="pt-2 list-disc pl-4">
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Analyze and optimize sells and invoices processes
+                </li>
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Create APIs with Node JS
+                </li>
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Design Optimized process with Figma
+                </li>
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Design Low fidelity designs{" "}
+                </li>
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Design High fidelity designs
+                </li>
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Design SQL model from Database.
+                </li>
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Documentation code.
+                </li>
+              </ul>
 
-            <p className="dark:text-[#cdbead] text-[#1f1c19] mt-2">
-              Tech Stack
-            </p>
-            <div>
-              <div className="flex bg-emerald-400 rounded-full w-fit justify-center py-1 px-2 items-center">
-                React
-                <BiLogoReact />
+              <p className="dark:text-[#cdbead] text-[#1f1c19] mt-2">
+                Tech Stack
+              </p>
+              <div>
+                <div className="flex bg-emerald-400 rounded-full w-fit justify-center py-1 px-2 items-center">
+                  React
+                  <BiLogoReact />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="bg-white dark:bg-slate-800 rounded p-4">
-            <h3 className="dark:text-[#cdbead] text-[#1f1c19] text-2xl font-bold tracking-tighter">
-              Ui Developer at Nivelics, Bogotá
-            </h3>
-            <p>
-              Acquired experience, developing stylized components with Styled
-              Components, and good quality standards such as Pixel Perfect have
-              allowed me to successfully finish two large projects with
-              television chains in Mexico and Colombia.
-            </p>
-            <p className="dark:text-[#cdbead] text-[#1f1c19] text-1xl font-thin tracking-tighter">
-              March 2023 - Present
-              <span></span>
-            </p>
-            <ul className="pt-2 list-disc pl-4">
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Analyze and estimate development times based on the proposed
-                designs
-              </li>
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Web prototype design according to the wireframes proposed by the
-                client
-              </li>
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Develop approved designs -based model.
-              </li>
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Constant communication with the client giving daily task status.
-              </li>
-            </ul>
+            <div className="bg-white dark:bg-slate-800 rounded p-4">
+              <h3 className="dark:text-[#cdbead] text-[#1f1c19] text-2xl font-bold tracking-tighter">
+                Ui Developer at Nivelics, Bogotá
+              </h3>
+              <p>
+                Acquired experience, developing stylized components with Styled
+                Components, and good quality standards such as Pixel Perfect
+                have allowed me to successfully finish two large projects with
+                television chains in Mexico and Colombia.
+              </p>
+              <p className="dark:text-[#cdbead] text-[#1f1c19] text-1xl font-thin tracking-tighter">
+                March 2023 - Present
+                <span></span>
+              </p>
+              <ul className="pt-2 list-disc pl-4">
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Analyze and estimate development times based on the proposed
+                  designs
+                </li>
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Web prototype design according to the wireframes proposed by
+                  the client
+                </li>
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Develop approved designs -based model.
+                </li>
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Constant communication with the client giving daily task
+                  status.
+                </li>
+              </ul>
 
-            <p className="dark:text-[#cdbead] text-[#1f1c19] mt-2">
-              Tech Stack
-            </p>
-            <div>
-              <div className="flex bg-emerald-400 rounded-full w-fit justify-center py-1 px-2 items-center">
-                React
-                <BiLogoReact />
+              <p className="dark:text-[#cdbead] text-[#1f1c19] mt-2">
+                Tech Stack
+              </p>
+              <div>
+                <div className="flex bg-emerald-400 rounded-full w-fit justify-center py-1 px-2 items-center">
+                  React
+                  <BiLogoReact />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="bg-white dark:bg-slate-800 rounded p-4">
-            <h3 className="dark:text-[#cdbead] text-[#1f1c19] text-2xl font-bold tracking-tighter">
-              Web Developer at Business Process Management Latinoamerica, Bogotá
-            </h3>
-            <p>
-              Experience developing the bpm-lat.com website from the creative
-              stage to the internal development of the site.
-            </p>
-            <p className="dark:text-[#cdbead] text-[#1f1c19] text-1xl font-thin tracking-tighter">
-              March 2023 - Present
-              <span></span>
-            </p>
-            <ul className="pt-2 list-disc pl-4">
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Design and develop the internal website.
-              </li>
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Design BPM forms for public entities.
-              </li>
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Develop BPMN processes for public entities.
-              </li>
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                Code and infrastructure maintenance.
-              </li>
-              <li className="dark:text-[#cdbead] text-[#1f1c19]">
-                ETLs development and API rest services.
-              </li>
-            </ul>
+            <div className="bg-white dark:bg-slate-800 rounded p-4">
+              <h3 className="dark:text-[#cdbead] text-[#1f1c19] text-2xl font-bold tracking-tighter">
+                Web Developer at Business Process Management Latinoamerica,
+                Bogotá
+              </h3>
+              <p>
+                Experience developing the bpm-lat.com website from the creative
+                stage to the internal development of the site.
+              </p>
+              <p className="dark:text-[#cdbead] text-[#1f1c19] text-1xl font-thin tracking-tighter">
+                March 2023 - Present
+                <span></span>
+              </p>
+              <ul className="pt-2 list-disc pl-4">
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Design and develop the internal website.
+                </li>
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Design BPM forms for public entities.
+                </li>
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Develop BPMN processes for public entities.
+                </li>
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  Code and infrastructure maintenance.
+                </li>
+                <li className="dark:text-[#cdbead] text-[#1f1c19]">
+                  ETLs development and API rest services.
+                </li>
+              </ul>
 
-            <p className="dark:text-[#cdbead] text-[#1f1c19] mt-2">
-              Tech Stack
-            </p>
-            <div>
-              <div className="flex bg-emerald-400 rounded-full w-fit justify-center py-1 px-2 items-center">
-                React
-                <BiLogoReact />
+              <p className="dark:text-[#cdbead] text-[#1f1c19] mt-2">
+                Tech Stack
+              </p>
+              <div>
+                <div className="flex bg-emerald-400 rounded-full w-fit justify-center py-1 px-2 items-center">
+                  React
+                  <BiLogoReact />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
